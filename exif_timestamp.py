5820e8f9554b80628ed57a6f73c03cbe1acf5c07
Bart Stroeken <bart.stroeken@gmail.com>
2019-06-20 20:33:24 +0200

    Updated some debug info and removed ipdb

--- exif_timestamp.py
+++ exif_timestamp.py
@@ -13,9 +13,11 @@ def image_modification_date_from_exif(imagename):
     :param imagename: name of the image to handle
     """
     if os.path.exists(imagename):
+        print("Parsing {0}...".format(imagename))
         filestream = open(imagename, 'rb')
         tags = exifread.process_file(filestream)
         start_time = tags.get('EXIF DateTimeOriginal')
+        print(start_time)
         timestamp = datetime.strptime(start_time.values, "%Y:%m:%d %H:%M:%S")
         mt = int(time.mktime(timestamp.timetuple()))
         os.utime(imagename, (mt, mt))
@@ -41,32 +43,34 @@ args = parser.parse_args()
 handled = False
 if args.path:
     # Handle a full directory.
+    print ("Handling {0}...\n".format(args.path))
     if os.path.exists(args.path):
         files = os.listdir(args.path)
         images = [img for img in files if '.jpg' in img.lower()]
-        print "Found {0} images in {1}".format(len(images), args.path)
+        print("Found {0} images in {1}".format(len(images), args.path))
         succeeded = 0
 
         for image in images:
             imagepath = os.path.abspath(os.path.join(args.path, image))
+            print("Handling {0}...\n".format(imagepath))
             res = image_modification_date_from_exif(imagepath)
             succeeded += 1
         if succeeded > 0:
-            print "Handled {0} images. ".format(succeeded)
+            print("Handled {0} images. \n".format(succeeded))
             handled = True
     else:
-        print "Invalid path: {0} ".format(args.path)
+        print("Invalid path: {0} \n".format(args.path))
 
 if args.image:
     # Handle an individual image.
     if os.path.exists(args.image):
         res = image_modification_date_from_exif(args.image)
-        print "Set the date of {0} to {1}".format(args.image, res.isoformat())
+        print("Set the date of {0} to {1}".format(args.image, res.isoformat()))
         handled = True
     else:
-        print "Image {0} not found!".format(args.image)
+        print("Image {0} not found!".format(args.image))
 
 if not handled:
-    print parser.print_help()
-    print ""
-    print "Nothing was done; did you specify image or a path? Do they exist? "
+    print (parser.print_help())
+    print ("")
+    print ("Nothing was done; did you specify image or a path? Do they exist? ")
